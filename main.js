ba08c3b81f0d82433901ba41fdbbdc5f546ad544
vitalijs <117673160+VitalijsVadinskis@users.noreply.github.com>
2023-08-01 18:07:36 +0300

    remake css with grid insted of flex

--- main.js
+++ main.js
@@ -1,23 +1,28 @@
-const sizeButton = document.body.querySelectorAll('input')
-const container = document.getElementById('container')
-
-
-sizeButton.forEach(e => {
-
-    e.addEventListener('click', function(){
-         console.log(e.value)
-         for(i = 0; i < e.value; i++){
-            let div = document.createElement('div')
-            div.className = 'square'
-            container.appendChild(div)
-
-
-         }
-    })
-    
-    
+const grid = document.getElementById('container')
+grid.style.gridTemplateColumns = 'repeat(16, 1fr)';
+grid.style.gridTemplateRows = 'repeat(16, 1fr)';
 
 
 
-})
 
+function createGrid() {
+    for(i = 0; i < 256; i++){
+        let square = document.createElement('div')
+        grid.appendChild(square)
+    }
+}
+
+createGrid()
+// const squares = document.getElementsByClassName('square')
+
+// sizeButton.forEach(e => {
+
+//     e.addEventListener('click', function(){
+//          container.innerHTML = ''
+//          for(i = 0; i < e.value; i++){
+//             let div = document.createElement('div')
+//             div.className = 'square'
+//             container.appendChild(div)
+//          }
+//     })
+// })
